b091d254d9e418a86672631301407ca57bd8b26e
YaimaGitHub <tvalacarta33@gmail.com>
2026-01-13 05:00:18 -0600

    j
    j

--- src/frontend/contexts/index.js
+++
@@ -1,5 +0,0 @@
-import AuthContextProvider from './AuthContextProvider.jsx';
-import FiltersContextProvider from './FiltersContextProvider.jsx';
-import ProductsContextProvider from './ProductsContextProvider';
-
-export { AuthContextProvider, ProductsContextProvider, FiltersContextProvider };
